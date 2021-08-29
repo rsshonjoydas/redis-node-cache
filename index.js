b0b7a92c30cf65d9e6646a4c124e13e46e37ae94
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-29 18:32:07 +0600

    redis get data

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const REDIS_PORT = process.env.REDIS_PORT || "redis://127.0.0.1:6379";
 const client = redis.createClient(REDIS_PORT);
 
 client.set = util.promisify(client.set)
+client.get = util.promisify(client.get);
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.post("/", async (req, res) => {
   const { key, value } = req.body;
   const response = await client.set(key, value)
   res.json(response)
+})
+
+app.get("/", async (req, res) => {
+  const { key } = req.body;
+  const value = await client.get(key)
+  res.json(value)
 })
 
 app.listen(PORT, () => {
