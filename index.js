f192b64670509cd1b6f9837ce878e8c0ab709787
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-29 18:36:43 +0600

    fetch data with axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const redis = require("redis")
 const util = require("util")
+const axios = require("axios")
 
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || "redis://127.0.0.1:6379";
@@ -12,7 +13,7 @@ client.get = util.promisify(client.get);
 const app = express();
 app.use(express.json());
 
-app.post("/", async (req, res) => {
+app.post("/", async(req, res) => {
   const { key, value } = req.body;
   const response = await client.set(key, value)
   res.json(response)
@@ -22,6 +23,14 @@ app.get("/", async (req, res) => {
   const { key } = req.body;
   const value = await client.get(key)
   res.json(value)
+})
+
+app.get("/posts/:id", async(req, res) => {
+  const { id } = req.params;
+
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+  return res.json(response.data)
 })
 
 app.listen(PORT, () => {
